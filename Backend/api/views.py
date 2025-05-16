518ba8757017ca2ffbcc68cde70e2f58926bf5a6
IamHarriiii <iamharriiiii@gmail.com>
2025-05-16 20:44:16 +0530

    update views n serializers of the api

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CodeInputSerializer
+from .utils import run_model_inference
 
-# Create your views here.
+class PredictBugView(APIView):
+    def post(self, request):
+        serializer = CodeInputSerializer(data=request.data)
+        if serializer.is_valid():
+            code = serializer.validated_data["code"]
+            result = run_model_inference(code, mode="bug")
+            return Response(result)
+        return Response(serializer.errors, status=400)
+
+class OptimizeCodeView(APIView):
+    def post(self, request):
+        serializer = CodeInputSerializer(data=request.data)
+        if serializer.is_valid():
+            code = serializer.validated_data["code"]
+            result = run_model_inference(code, mode="optimize")
+            return Response(result)
+        return Response(serializer.errors, status=400)
+
+class GenerateDocsView(APIView):
+    def post(self, request):
+        serializer = CodeInputSerializer(data=request.data)
+        if serializer.is_valid():
+            code = serializer.validated_data["code"]
+            result = run_model_inference(code, mode="doc")
+            return Response(result)
+        return Response(serializer.errors, status=400)
